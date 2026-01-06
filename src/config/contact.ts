c0b7fd90404a81a59250e5dae386552b3a689011
l-nikola <legay.nikola.pro@gmail.com>
2026-01-06 18:44:29 +0100

    feat : add config file

---
+++ src/config/contact.ts
@@ -0,0 +1,17 @@
+export const contact = {
+  phone: {
+    display: "06 30 14 56 82",
+    link: "+33630145682",
+  },
+  email: "legay.nikola.pro@gmail.com",
+  address: {
+    street: "1801 route des Sablons",
+    zip: "76530",
+    city: "Yville-sur-Seine",
+    country: "France",
+  },
+  social: {
+    github: "https://github.com/l-nikola",
+    linkedin: "https://www.linkedin.com/in/nikola-legay-090b02283/",
+  },
+};
